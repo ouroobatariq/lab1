df9162c487f00b1132ee146d1c85cf9aa4933100
ouroobatariq <62900041+ouroobatariq@users.noreply.github.com>
2020-06-20 16:42:46 +0500

    Add files via upload

---
+++ src/main/resources/static/precache-manifest.7021f0f22605be12fd8da587c7671a30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df893102ae871950361771c6bd170d7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6152a3a788aa3cd3f3e0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d716f323edaa0ee9ccb0",
+    "url": "/static/css/main.00d4352e.chunk.css"
+  },
+  {
+    "revision": "6152a3a788aa3cd3f3e0",
+    "url": "/static/js/2.2317540d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2317540d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d716f323edaa0ee9ccb0",
+    "url": "/static/js/main.20f95403.chunk.js"
+  },
+  {
+    "revision": "fe35f00a91369b1ee7d4",
+    "url": "/static/js/runtime-main.3f900b9c.js"
+  }
+]);
